ae1ec62f17130948c9f88932735b1f56e1ca3985
JoelUCOP <joel.hagedorn@ucop.edu>
2016-03-08 13:43:53 -0800

    move details polyfill to new file, link within pages

---
+++ app/js/details-polyfill.js
@@ -0,0 +1,46 @@
+// ##### Details Element Polyfill ##### //
+
+// Detect via Modernizr if details element is supported in a browser:
+
+if (Modernizr.details) {
+
+  // Details element supported:
+
+  $('details').attr('aria-expanded', 'false');
+
+  if ($('details').is('[open]')) {
+    $('[open]').attr('aria-expanded', 'true');
+  }
+
+  $('summary').click(function(){
+    if ($(this).parent().is('[open]')) {
+      $(this).parent().attr('aria-expanded', 'false');
+    } else {
+      $(this).parent().attr('aria-expanded', 'true');
+    }
+  });
+
+} else {
+
+  // Details element not supported:
+
+  $('summary').siblings().hide();
+  $('details').attr('aria-expanded', 'false');
+  
+  if ($('details').is('[open]')) {
+    $('[open]').children().show();
+    $('[open]').attr('aria-expanded', 'true');
+  }
+
+  $('summary').click(function(){
+    $(this).siblings().toggle();
+
+    if ($(this).parent().is('[open]')) {
+      $(this).parent().removeAttr('open');
+      $(this).parent().attr('aria-expanded', 'false');
+    } else {
+      $(this).parent().attr('open', '');
+      $(this).parent().attr('aria-expanded', 'true');
+    }
+  });
+}
