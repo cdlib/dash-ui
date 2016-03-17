58c4a3430a988603325c6b1f9374e676897d4f3a
JoelUCOP <joel.hagedorn@ucop.edu>
2016-03-17 16:33:40 -0700

    add table detail button toggle js and classes

--- app/js/main.js
+++ app/js/main.js
@@ -36,4 +36,21 @@ $(document).ready(function(){
     $(this).siblings('label').addClass('c-input__label--required');
   });
 
+  // ***** Toggle Table Heading Buttons ***** //
+
+  $('.js-table-heading__button-hide').hide();
+  $('.js-table__details--hide').toggleClass('js-table__details--hide js-table__details--show');
+
+  $('.js-table-heading__button-show').click(function(){
+    $('.js-table-heading__button-show').hide();
+    $('.js-table-heading__button-hide').show();
+    $('.js-table__details--hide').toggleClass('js-table__details--hide js-table__details--show');
+  });
+
+  $('.js-table-heading__button-hide').click(function(){
+    $('.js-table-heading__button-hide').hide();
+    $('.js-table-heading__button-show').show();
+    $('.js-table__details--show').toggleClass('js-table__details--show js-table__details--hide');
+  });
+
 }); // Close $(document).ready(function()
