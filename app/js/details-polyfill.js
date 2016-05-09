6f0a430805be3d7c94a9cf36bb8f1127d1d239c7
JoelUCOP <joel.hagedorn@ucop.edu>
2016-05-09 14:38:55 -0700

    wrap details-polyfill js in document.ready jquery function

--- app/js/details-polyfill.js
+++ app/js/details-polyfill.js
@@ -1,48 +1,52 @@
 // ##### Details Element Polyfill ##### //
 
-// Detect via Modernizr if details element is supported in a browser:
+$(document).ready(function(){
 
-if (Modernizr.details) {
+  // Detect via Modernizr if details element is supported in a browser:
 
-  // Details element supported:
+  if (Modernizr.details) {
 
-  $('details').attr('aria-expanded', 'false');
-  $('summary').attr('role', 'button');
+    // Details element supported:
 
-  if ($('details').is('[open]')) {
-    $('[open]').attr('aria-expanded', 'true');
+    $('details').attr('aria-expanded', 'false');
+    $('summary').attr('role', 'button');
+
+    if ($('details').is('[open]')) {
+      $('[open]').attr('aria-expanded', 'true');
+    }
+
+    $('summary').click(function(){
+      if ($(this).parent().is('[open]')) {
+        $(this).parent().attr('aria-expanded', 'false');
+      } else {
+        $(this).parent().attr('aria-expanded', 'true');
+      }
+    });
+
+  } else {
+
+    // Details element not supported:
+
+    $('details').attr('aria-expanded', 'false');
+    $('summary').attr('role', 'button');
+    $('summary').siblings().hide();
+
+    if ($('details').is('[open]')) {
+      $('[open]').children().show();
+      $('[open]').attr('aria-expanded', 'true');
+    }
+
+    $('summary').click(function(){
+      $(this).siblings().toggle();
+
+      if ($(this).parent().is('[open]')) {
+        $(this).parent().removeAttr('open');
+        $(this).parent().attr('aria-expanded', 'false');
+      } else {
+        $(this).parent().attr('open', '');
+        $(this).parent().attr('aria-expanded', 'true');
+      }
+    });
   }
 
-  $('summary').click(function(){
-    if ($(this).parent().is('[open]')) {
-      $(this).parent().attr('aria-expanded', 'false');
-    } else {
-      $(this).parent().attr('aria-expanded', 'true');
-    }
-  });
-
-} else {
-
-  // Details element not supported:
-
-  $('details').attr('aria-expanded', 'false');
-  $('summary').attr('role', 'button');
-  $('summary').siblings().hide();
-
-  if ($('details').is('[open]')) {
-    $('[open]').children().show();
-    $('[open]').attr('aria-expanded', 'true');
-  }
-
-  $('summary').click(function(){
-    $(this).siblings().toggle();
-
-    if ($(this).parent().is('[open]')) {
-      $(this).parent().removeAttr('open');
-      $(this).parent().attr('aria-expanded', 'false');
-    } else {
-      $(this).parent().attr('open', '');
-      $(this).parent().attr('aria-expanded', 'true');
-    }
-  });
-}
+}); // Close $(document).ready(function()
